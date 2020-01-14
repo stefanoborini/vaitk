879c4d95238607ff38bf3066ff5a2d56ae827b0c
Stefano Borini <stefano.borini@gmail.com>
2020-01-14 22:27:03 +0000

    Refactored Event to use traits and added test

---
+++ tests/vaitk/core/test_Event.py
@@ -0,0 +1,17 @@
+from vaitk.core import Event
+from vaitk.core.enums import EventType
+
+
+def test_event_initialisation():
+    ev = Event(EventType.Timer)
+
+    assert ev.event_type == EventType.Timer
+    assert not ev.accepted
+
+    ev.accept()
+
+    assert ev.accepted
+
+    ev.ignore()
+
+    assert not ev.accepted
