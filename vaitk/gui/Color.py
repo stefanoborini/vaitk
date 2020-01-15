7ef9d6169d6ce3401dc0e64355700acd39c96653
Stefano Borini <stefano.borini@gmail.com>
2020-01-15 23:02:34 +0000

    Fix color distance and incorrect test

--- vaitk/gui/Color.py
+++ vaitk/gui/Color.py
@@ -1,20 +1,29 @@
 from traitlets import HasTraits, Int
+import math
 
 
 class Color(HasTraits):
-    red = Int()
-    green = Int()
-    blue = Int()
+    """
+    Describes a color in RGB format
+    """
+    red = Int(min=0, max=255)
+    green = Int(min=0, max=255)
+    blue = Int(min=0, max=255)
 
     def __init__(self, red, green, blue):
         super().__init__(red=red, green=green, blue=blue)
 
     def hex_string(self):
+        """Returns the color as a hexadecimal string"""
         return "{:02X}{:02X}{:02X}".format(self.red, self.green, self.blue)
 
     @staticmethod
     def distance(color1, color2):
-        return (
+        """
+        Returns the distance between two colors as calculated by
+        the sum of the squared differences between each channel.
+        """
+        return math.sqrt(
                 (color1.red - color2.red)**2 +
                 (color1.green - color2.green)**2 +
                 (color1.blue - color2.blue)**2
