c7c1a07983165d295939ab0649398c68579a9602
Stefano Borini <stefano.borini@gmail.com>
2020-03-21 21:02:42 +0000

    Added docstrings to TextScreenDriver

--- vaitk/core/drivers/text/text_screen_driver.py
+++ vaitk/core/drivers/text/text_screen_driver.py
@@ -17,21 +17,45 @@ class TextScreenDriver(ABCDriver):
     """
 
     def __init__(self, size):
-        self._size = Size(size[0], size[1])
+        """
+        Creates a driver for a text based screen with a given size
+
+        Args:
+            size: Size
+                The size of the screen.
+        """
+        if isinstance(size, Size):
+            self._size = size
+        else:
+            self._size = Size(size[0], size[1])
 
         self._cursor_pos = Point(0, 0)
         self._render_output = []
         self._queue = Queue()
-        self.erase()
 
     def init(self):
-        pass
+        """
+        Initializes the driver
+
+        Returns: None
+
+        """
+        self.erase()
 
     def deinit(self):
-        pass
+        """
+        Deinitialises the driver.
+
+        Returns: None
+
+        """
 
     @property
-    def num_colors(self):
+    def num_nominal_colors(self):
+        return 1
+
+    @property
+    def num_representable_colors(self):
         return 1
 
     @property
@@ -50,6 +74,22 @@ class TextScreenDriver(ABCDriver):
         self.erase()
 
     def write(self, pos, string, fg_color=None, bg_color=None):
+        """
+        Writes a string at a given position with given colors.
+
+        Args:
+            pos: Point
+                Where in the screen the string should be written,
+                top left corner.
+            string: str
+                String to write
+            fg_color: Color or None
+                The foreground color
+            bg_color: Color or None
+                The background color.
+
+        Returns: None
+        """
         for pos_x in range(len(string)):
             try:
                 self._render_output[pos.y][pos.x+pos_x] = string[pos_x]
@@ -57,35 +97,86 @@ class TextScreenDriver(ABCDriver):
                 logger.warning("Invalid write position : ", str(pos), string)
 
     def get_event(self):
+        """
+        Gets an event from the driver.
+
+        Returns: Event
+
+        """
         data = self._queue.get()
         return data
 
     def type_string(self, string):
+        """
+        Types the given string in the event buffer as a list of individual
+        characters.
+
+        Args:
+            string: str
+                the string to type
+
+        Returns: None
+
+        """
         self._queue.put(list(string))
 
     def dump(self):
+        """
+        Returns the content of the screen as a list of rows. Each row is a
+        list of characters.
+
+        Returns: list
+        """
         ret = list()
         ret.append(" "+"".join(list(itertools.islice(itertools.cycle(
-            list(map(str, list(range(10))))), self._size[0]+1))))
-        # print "+"*(self._size[0]+2)
+            list(map(str, list(range(10))))), self._size.width+1))))
         for i, r in enumerate(self._render_output):
             ret.append(str(i % 10)+''.join(r)+"+")
-        ret.append("+"*(self._size[0]+2))
+        ret.append("+"*(self._size.width+2))
         return ret
 
     def __str__(self):
-        return "\n".join(self.dump())
+        """
+        method for str() resolution.
 
-    def char_at(self, x, y):
-        return self._render_output[y][x]
+        Returns: str
 
-    def string_at(self, x, y, l):
-        return ''.join(self._render_output[y][x:x+l])
+        """
+        return "\n" + "\n".join(self.dump())
+
+    def char_at(self, pos):
+        """
+        Returns the character at the given position.
+        Args:
+            pos: Point
+                The position
+
+        Returns: a single character
+
+        """
+        return self._render_output[pos.y][pos.x]
+
+    def string_at(self, pos, length):
+        """
+        Returns the content at position pos of given length.
+
+        Args:
+            pos: Point
+                the position on the screen
+            length: int
+                the amount of chars to return
+
+        Returns: the string at the given position
+        """
+        return ''.join(self._render_output[pos.y][pos.x:pos.x + length])
 
     def erase(self):
+        """
+        Clears the whole area
+        """
         self._render_output.clear()
         for h in range(self.size.height):
             row = []
             self._render_output.append(row)
             for w in range(self.size.width):
-                row.append('.')
+                row.append(' ')
